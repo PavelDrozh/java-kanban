3a9f5a3d225b6b87747ccde056737e3ce30db2ae
Pavel Drozhzhachih <pavel.drozhzhachih@gmail.com>
2022-08-06 15:19:53 +0300

    feat: добавить реализацию класса InMemoryHistoryManager

---
+++ src/ru/yandex/practicum/tasktracker/InMemoryHistoryManager.java
@@ -0,0 +1,34 @@
+package ru.yandex.practicum.tasktracker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager {
+
+    private static final int MAX_HISTORY_SIZE = 10;
+    private static final int FIRST_INDEX = 0;
+    private final List<Task> history;
+
+    public InMemoryHistoryManager() {
+        history = new ArrayList<>();
+    }
+
+    @Override
+    public void add(Task task) {
+        if (task != null) {
+            history.add(task);
+            checkHistorySize();
+        }
+    }
+
+    private void checkHistorySize() {
+        if (history.size() > MAX_HISTORY_SIZE) {
+            history.remove(FIRST_INDEX);
+        }
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return history;
+    }
+}
